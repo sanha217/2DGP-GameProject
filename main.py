8451b132847c223c52e84e8f4d09d396b134fc08
LeeSanha <sanha7665@gmail.com>
2025-10-19 19:14:54 +0900

    메인 함수 스켈레톤 작성

---
+++ main.py
@@ -0,0 +1,15 @@
+from pico2d import *
+from Background import Background
+
+
+def reset_world():
+    pass
+
+def update_world():
+    pass
+
+def render_world():
+    pass
+
+if __name__ == '__main__':
+    pass
