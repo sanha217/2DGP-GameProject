73bd15732dc3a5007c804771f8e777c93940032a
LeeSanha <sanha7665@gmail.com>
2025-10-19 20:29:01 +0900

    기사 Idle 상태 완성

--- Knight.py
+++ Knight.py
@@ -1,25 +1,54 @@
 from pico2d import load_image
 
+canvas_width = 1280
+canvas_height = 720
+image_size = 2048
+frame_size = 128
+
+idle_y_offset = 8
+run_y_offset = 16
+
 class Idle:
     def __init__(self, knight):
-        pass
+        self.knight = knight
 
     def enter(self):
-        pass
+        self.knight.dir = 0
 
     def exit(self):
         pass
 
     def do(self):
-        pass
+        self.knight.frame = (self.knight.frame + 1) % 7
 
     def draw(self):
-        pass
+        if self.knight.face_dir == 1:
+            self.knight.image.clip_draw(
+                self.knight.frame * frame_size,
+                image_size - frame_size * idle_y_offset,
+                frame_size, frame_size,
+                self.knight.x, self.knight.y
+            )
+        else:
+            self.knight.image.clip_draw(
+                self.knight.frame * frame_size,
+                image_size - frame_size * idle_y_offset,
+                frame_size, frame_size,
+                self.knight.x, self.knight.y,
+                flip = 'h'
+            )
 
 
 class Knight:
     def __init__(self):
-        pass
+        self.x = canvas_width // 2 # 임시 시작 위치
+        self.y = 90
+        self.frame = 0
+        self.dir = 0
+        self.face_dir = 1
+        self.image = load_image('knight.png')
+
+        self.IDLE = Idle(self)
 
     def update(self):
         pass
