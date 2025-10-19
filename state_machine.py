d3ed87d3658650e04670566f7d85f74f04956d89
LeeSanha <sanha7665@gmail.com>
2025-10-19 20:03:31 +0900

    state_machine 스켈레톤 작성

---
+++ state_machine.py
@@ -0,0 +1,14 @@
+from event_to_string import event_to_string
+
+class StateMachine:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_state_event(self):
+        pass
