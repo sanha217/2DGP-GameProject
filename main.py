3cb3e287913c6220f51132f0e2dab82a73071af0
LeeSanha <sanha7665@gmail.com>
2025-10-19 20:07:54 +0900

    main에 handle_events() 추가 게임 종료 기능 추가

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from pico2d import *
 from Background import Background
 from Knight import Knight
 
+def handle_events():
+    global playing
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            playing = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            playing = False
+        else:
+            # 추가하기
+            pass
 
 def reset_world():
     global world
@@ -26,13 +38,17 @@ def render_world():
     update_canvas()
 
 if __name__ == '__main__':
+
+    playing = True
+
     canvas_width = 1280
     canvas_height = 720
 
     open_canvas(canvas_width, canvas_height)
     reset_world()
 
-    while True:
+    while playing:
+        handle_events()
         update_world()
         render_world()
         delay(0.01)
