73da0c7629c4311ae683e0682f439dea073acaf8
LeeSanha <sanha7665@gmail.com>
2025-10-19 20:12:19 +0900

    기사 Idle 상태 스켈레톤 추가

--- Knight.py
+++ Knight.py
@@ -1,5 +1,20 @@
 from pico2d import load_image
 
+class Idle:
+    def __init__(self, knight):
+        pass
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def do(self):
+        pass
+
+    def draw(self):
+        pass
 
 
 class Knight:
