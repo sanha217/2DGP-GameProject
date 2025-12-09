10a5471672d1285079e10e7609f91dda0d554f63
LeeSanha <sanha7665@gmail.com>
2025-12-10 03:59:15 +0900

    타이틀 화면 이미지 추가

--- title_mode.py
+++ title_mode.py
@@ -1,17 +1,25 @@
-
 from pico2d import *
 import game_framework
 import play_mode
 
+image = None
+font = None
+
+
 def enter():
-    global font
+    global image, font
+    image = load_image('title.png')
     try:
         font = load_font('ENCR10B.TTF', 50)
     except IOError:
         font = load_font('C:/Windows/Fonts/arial.ttf', 50)
 
+
 def exit():
-    pass
+    global image, font
+    del image
+    del font
+
 
 def handle_events():
     events = get_events()
@@ -24,17 +32,23 @@ def handle_events():
             elif event.key == SDLK_SPACE:
                 game_framework.change_mode(play_mode)
 
+
 def draw():
     clear_canvas()
-    font.draw(400, 400, "Knight vs Hornet", (255, 255, 255))
-    font.draw(350, 300, "Press SPACE to Start", (255, 255, 255))
+
+    image.draw(640, 360, 1280, 720)
+
+    font.draw(400, 100, "Press SPACE to Start", (255, 255, 255))
     update_canvas()
+
 
 def update():
     pass
 
+
 def pause():
     pass
+
 
 def resume():
     pass
