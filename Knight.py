522d7643d4439dc22330765ef3712ae5727036b3
LeeSanha <sanha7665@gmail.com>
2025-12-10 05:18:07 +0900

    기사 스킬 구현

--- Knight.py
+++ Knight.py
@@ -1,11 +1,12 @@
 from pico2d import load_image, get_time, draw_rectangle
 from sdl2 import SDL_KEYDOWN, SDL_KEYUP, SDL_GetKeyboardState, \
-    SDLK_a, SDLK_d, SDLK_w, SDLK_f, SDLK_g, \
+    SDLK_a, SDLK_d, SDLK_w, SDLK_f, SDLK_g, SDLK_t, \
     SDL_SCANCODE_A, SDL_SCANCODE_D, SDL_SCANCODE_W
 
 from state_machine import StateMachine
 from pico2d import load_image, get_time, draw_rectangle, load_wav
 import game_framework
+import game_world
 
 # 설정 변수
 
@@ -28,6 +29,9 @@ ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 TIME_PER_ATTACK = 0.15
 ATTACK_PER_TIME = 1.0 / TIME_PER_ATTACK
 
+TIME_PER_SKILL = 0.5
+SKILL_PER_TIME = 1.0 / TIME_PER_SKILL
+
 # [y오프셋. 프레임 오프셋]
 idle_offset = [9, 7]
 run_offset = [1, 9]
@@ -35,7 +39,7 @@ jump_offset = [10, 12]
 dash_offset = [5, 7]
 attack_offset = [4, 8]
 attack_effect_offset = [13, 13]
-
+skill_offset = [11, 16]
 
 # 이벤트 체크 함수 (키 변경 적용: A, D, W, F, G)
 
@@ -66,8 +70,109 @@ def f_down(e):  # 대시
 def g_down(e):  # 공격
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_g
 
+def t_down(e): # 스킬
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_t
+
+# 투사체 클래스
+class KnightSkill:
+    image = None
+
+    def __init__(self, x, y, dir):
+        if KnightSkill.image == None:
+            KnightSkill.image = load_image('knight skill.png')
+        self.x, self.y, self.dir = x, y, dir
+        self.speed = RUN_SPEED_PPS * 2.0
+        self.frame = 0
+
+    def update(self):
+        self.x += self.dir * self.speed * game_framework.frame_time
+        self.frame = (self.frame + 3 * ACTION_PER_TIME * game_framework.frame_time) % 3
+
+        if self.x < -100 or self.x > canvas_width + 100:
+            game_world.remove_object(self)
+
+    def draw(self):
+        frame_idx = int(self.frame)
+        bottom = (2 - frame_idx) * 128
+
+        if self.dir == 1:
+            self.image.clip_draw(0, bottom, 512, 128, self.x, self.y)
+        else:
+            self.image.clip_composite_draw(0, bottom, 512, 128, 0, 'h', self.x, self.y, 512, 128)
+
+    def get_body_box(self):
+        return self.x - 100, self.y - 40, self.x + 100, self.y + 40
+
+    def handle_collision(self, group, other):
+        pass
+
 
 # 상태 클래스
+
+class Skill:
+    def __init__(self, knight):
+        self.knight = knight
+        self.has_fired = False
+
+    def enter(self, event):
+        self.knight.frame = 0
+        self.has_fired = False
+
+    def exit(self):
+        pass
+
+    def do(self):
+        total_frames = skill_offset[1]
+        self.knight.frame += total_frames * SKILL_PER_TIME * game_framework.frame_time
+
+        time_scale = game_framework.frame_time * 60
+        self.knight.y += self.knight.y_velocity * time_scale
+        self.knight.y_velocity -= self.knight.gravity * time_scale
+
+        if self.knight.y <= ground:
+            self.knight.y = ground
+            self.knight.y_velocity = 0
+
+        if not self.has_fired and int(self.knight.frame) >= 8:
+            skill = KnightSkill(self.knight.x, self.knight.y, self.knight.face_dir)
+            game_world.add_object(skill, 1)
+            self.has_fired = True
+
+        if int(self.knight.frame) >= total_frames:
+            self.knight.frame = total_frames - 1
+
+            if self.knight.y > ground:
+                self.knight.state_machine.cur_state = self.knight.JUMP
+            else:
+                self.knight.state_machine.cur_state = self.knight.IDLE
+
+            self.knight.state_machine.cur_state.enter(('SKILL_END', 0))
+
+    def draw(self):
+        cur_frame = int(self.knight.frame)
+        if self.knight.face_dir == 1:
+            self.knight.image.clip_draw(
+                cur_frame * frame_size,
+                image_size - frame_size * skill_offset[0],
+                frame_size, frame_size,
+                self.knight.x, self.knight.y,
+                frame_size, frame_size
+            )
+        else:
+            self.knight.image.clip_composite_draw(
+                cur_frame * frame_size,
+                image_size - frame_size * skill_offset[0],
+                frame_size, frame_size,
+                0, 'h',
+                self.knight.x, self.knight.y,
+                frame_size, frame_size
+            )
+
+    def get_body_box(self):
+        return self.knight.get_body_box()
+
+    def get_attack_box(self):
+        return None
 
 class Attack:
     def __init__(self, knight):
@@ -432,6 +537,7 @@ class Knight:
         self.JUMP = Jump(self)
         self.DASH = Dash(self)
         self.ATTACK = Attack(self)
+        self.SKILL = Skill(self)
         self.state_machine = StateMachine(
             self.IDLE,
             {
@@ -442,7 +548,8 @@ class Knight:
                     a_up: self.RUN,
                     w_down: self.JUMP,
                     f_down: self.DASH,
-                    g_down: self.ATTACK
+                    g_down: self.ATTACK,
+                    t_down: self.SKILL
                 },
                 self.RUN: {
                     d_down: self.IDLE,
@@ -451,7 +558,8 @@ class Knight:
                     a_up: self.IDLE,
                     w_down: self.JUMP,
                     f_down: self.DASH,
-                    g_down: self.ATTACK
+                    g_down: self.ATTACK,
+                    t_down: self.SKILL
                 },
                 self.JUMP: {
                     d_down: self.JUMP,
@@ -459,11 +567,14 @@ class Knight:
                     d_up: self.JUMP,
                     a_up: self.JUMP,
                     f_down: self.DASH,
-                    g_down: self.ATTACK
+                    g_down: self.ATTACK,
+                    t_down: self.SKILL
                 },
                 self.DASH: {
                 },
                 self.ATTACK: {
+                },
+                self.SKILL: {
                 }
             }
         )
